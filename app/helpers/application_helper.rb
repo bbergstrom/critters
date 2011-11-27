ecd92651a281f6c7055e5b414199c0d8da22f21c
Brian Bergstrom <boilingbergstrom@gmail.com>
2011-11-27 00:09:51 -0600

    Adding current page classes to nav and styles to match.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
   def controller_css_class 
     return "#{params[:controller]}-#{params[:action]}"
   end
+
+  def current_page_class(options)
+    if current_page? options
+      return 'current-page'
+    end
+    return 'not-current-page'
+  end
 end
