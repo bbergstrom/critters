c244357c3935343113819274f12e7e382d9f2627
Brian Bergstrom <boilingbergstrom@gmail.com>
2011-11-27 01:41:07 -0600

    Updating order of top critters to use quality as well.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     # The top and most recent 5 critters for the homepage feed.
-    @top_critters = Critter.find(:all, :limit => 5, :order => 'level DESC')
+    @top_critters = Critter.find(:all, :limit => 5, :order => 'level DESC, quality DESC')
     @recent_critters = Critter.find(:all, :limit => 5, :order => 'created_at DESC')
   end
 
