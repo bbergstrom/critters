ca43bac8ce17960cbf635eb22b9e2dd0a512c8dd
Brian Bergstrom <boilingbergstrom@gmail.com>
2011-11-27 02:57:42 -0600

    Updating home page lists.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
     # The top and most recent 5 critters for the homepage feed.
-    @top_critters = Critter.find(:all, :limit => 5, :order => 'level DESC, quality DESC')
-    @recent_critters = Critter.find(:all, :limit => 5, :order => 'created_at DESC')
+    @top_critters = Critter.find(:all, :limit => 10, :order => 'level DESC, quality DESC')
+    @recent_critters = Critter.find(:all, :limit => 10, :order => 'created_at DESC')
   end
 
   def about
