82c76a646b56cac9bafe81413b9c106eada3e12e
Brian Bergstrom <boilingbergstrom@gmail.com>
2011-12-02 23:57:01 -0600

    Fixing breeding data selection.

--- app/controllers/breeding_controller.rb
+++ app/controllers/breeding_controller.rb
@@ -1,7 +1,7 @@
 class BreedingController < ApplicationController
   def new
-    @males = Critter.where(:sex => 'M').order('level DESC, quality DESC, name ASC')
-    @females = Critter.where(:sex => 'F').order('level DESC, quality DESC, name ASC')
+    @males = current_user.critters.where(:sex => 'M').order('level DESC, quality DESC, name ASC')
+    @females = current_user.critters.where(:sex => 'F').order('level DESC, quality DESC, name ASC')
   end
 
   def create
@@ -15,8 +15,8 @@ class BreedingController < ApplicationController
     if @child.save
       redirect_to(@child, :notice => 'A new critter was bred!')
     else
-      @males = Critter.where(:sex => 'M').order('level DESC, quality DESC')
-      @females = Critter.where(:sex => 'F').order('level DESC, quality DESC')
+      @males = current_user.critters.where(:sex => 'M').order('level DESC, quality DESC')
+      @females = current_user.critters.where(:sex => 'F').order('level DESC, quality DESC')
       flash.now[:error] = 'The critters were not in the mood to breed today.'
       render :action => 'new'
     end
