c86eb3876fc406b101290ad6840d424c08a9751c
Brian Bergstrom <boilingbergstrom@gmail.com>
2011-12-02 22:58:18 -0600

    Fixing limit logic.

--- lib/critter_limit_validator.rb
+++ lib/critter_limit_validator.rb
@@ -1,6 +1,6 @@
 class CritterLimitValidator < ActiveModel::Validator
   def validate(record)
-    if !record.user.nil? && record.user.critters.count > 20
+    if !record.user.nil? && record.user.critters.count >= 20
       record.errors[:base] << "You have reached the max number(20) of critters!"
     end
   end
